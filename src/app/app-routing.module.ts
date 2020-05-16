452d62542e03bed1940077740a870d0d882dbffd
swarup-bhol <swarupbhol34@gmail.com>
2020-05-16 17:19:19 +0530

    All Required Component Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './Medsol-Common/page-not-found/page-not-found.component';
+import { LoginComponent } from './Medsol-Form/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Medsol-Form/login/login.component';
+import { RegisterComponent } from './Medsol-Form/register/register.component';
+import { ForgotPasswordComponent } from './Medsol-Form/forgot-password/forgot-password.component';
+import { ProfileInfoComponent } from './Medsol-Form/profile-info/profile-info.component';
+import { UploadDocumentComponent } from './Medsol-Form/upload-document/upload-document.component';
+import { ProfilePageComponent } from './Medsol-Profile/profile-page/profile-page.component';
+import { EditProfileComponent } from './Medsol-Profile/edit-profile/edit-profile.component';
+import { PageNotFoundComponent } from './Medsol-Common/page-not-found/page-not-found.component';
+import { ConfirmDialogComponent } from './Medsol-Common/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    ProfileInfoComponent,
+    UploadDocumentComponent,
+    ProfilePageComponent,
+    EditProfileComponent,
+    PageNotFoundComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
