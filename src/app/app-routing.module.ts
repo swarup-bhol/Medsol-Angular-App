1e38f4e9e5beba35151148dc0f2c64040e90482e
swarup-bhol <swarupbhol34@gmail.com>
2020-05-17 12:52:07 +0530

    Updated login signup and forgot password component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './Medsol-Common/page-not-found/page-not-found.component';
 import { LoginComponent } from './Medsol-Form/login/login.component';
 import { DashBoardComponent } from './Medsol-Dashboard/dash-board/dash-board.component';
+import { RegisterComponent } from './Medsol-Form/register/register.component';
+import { ForgotPasswordComponent } from './Medsol-Form/forgot-password/forgot-password.component';
 
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
+  { path: "signup", component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: "", component: DashBoardComponent },
   { path: '404', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/404' }
